ade97285ae7e52a0c87651432c73a9acba06088b
thefoxyjay <thefoxyjay@users.noreply.github.com>
2020-12-27 17:15:59 -0500

    Added axios intergration so it shows the weather

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Search from "./Search.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> How's the Weather?</h1>
+        <br />
+        <Search />
+        <br />
+        <Weather city="Tokyo" />
       </header>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    return `The weather in ${response.data.name} is ${response.data.main.temp}°F`;
+  }
+
+  const apiKey = "c99487f69d19999bb56503b8673bc536";
+  const units = "imperial";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={5000}
+    />
+  );
+}
