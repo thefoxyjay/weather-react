6992cdba75a3af0709714932984587b16b259100
thefoxyjay <thefoxyjay@users.noreply.github.com>
2020-12-29 23:55:11 -0600

    edited api and removed search.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import "./App.css";
 import "./Weather.css";
 import Weather from "./Weather.js";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Search from "./Search.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <header> How's the Weather?</header>
-        <Search />
         <br />
-        <Weather city="" />
+        <Weather defaultCity="Nashville" />
         <footer>
           This was coded by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,33 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import Loader from "react-loader-spinner";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <h1>Nashville</h1>
-      <ul>
-        <li>Wednesday 09:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          <span className="temperature">12</span>
-          <span className="units">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humiduty: 45%</li>
-            <li>Wind: 13mph</li>
-          </ul>
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+  function search() {
+    const apiKey = "c99487f69d19999bb56503b8673bc536";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Thursday 09:00</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            {weatherData.icon}
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="units">°F</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}</li>
+              <li>Wind: {weatherData.wind}mph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
